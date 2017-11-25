fc87b8a8d8a58129ef0563ad7944aaafdf6a3e09
Jason Hanna <jason.hanna01@gmail.com>
2017-11-25 15:04:53 -0500

    Add date parser

--- lib/enigma/keygen.ex
+++ lib/enigma/keygen.ex
@@ -1,16 +1,16 @@
 require IEx;
 
 defmodule Enigma.Keygen  do
-  alias Enigma.Offset
+  alias Enigma.Rotation
 
   def generate_key do
     key = Enum.random(10000..99999)
           |> Integer.to_string
   end
 
-  def get_rotations_from_key(key, offsets) do
-    offsets = %{
-      offsets |
+  def get_rotations_from_key(key, rotations) do
+    rotations = %{
+      rotations |
       A: "#{String.slice(key, 0..1)}",
       B: "#{String.slice(key, 1..2)}",
       C: "#{String.slice(key, 2..3)}",
--- test/keygen_test.exs
+++ test/keygen_test.exs
@@ -1,5 +1,3 @@
-require IEx;
-
 defmodule KeygenTest do
   use ExUnit.Case
   alias Enigma.Keygen
@@ -9,13 +7,13 @@ defmodule KeygenTest do
     assert String.length(Keygen.generate_key()) == 5
   end
 
-  test "it generates offsets from given key" do
+  test "it generates rotations from given key" do
     key = Keygen.generate_key()
-    offsets = Keygen.get_rotations_from_key(key, %Rotation{})
-    assert Map.get(offsets, :A) == String.slice(key, 0..1)
-    assert Map.get(offsets, :B) == String.slice(key, 1..2)
-    assert Map.get(offsets, :C) == String.slice(key, 2..3)
-    assert Map.get(offsets, :D) == String.slice(key, 3..4)
+    rotations = Keygen.get_rotations_from_key(key, %Rotation{})
+    assert Map.get(rotations, :A) == String.slice(key, 0..1)
+    assert Map.get(rotations, :B) == String.slice(key, 1..2)
+    assert Map.get(rotations, :C) == String.slice(key, 2..3)
+    assert Map.get(rotations, :D) == String.slice(key, 3..4)
   end
 end
 
